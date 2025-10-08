294acdb1c0690e617a9f92797f40f417c0cc299c
Peter Grman <peter.grman@gmail.com>
2025-10-09 01:56:42 +0200

    Initial modules

---
+++ custom_components/virtual_devices/const.py
@@ -0,0 +1,3 @@
+"""Constants for the Virtual Devices integration."""
+
+DOMAIN = "virtual_devices"
---
+++ custom_components/virtual_devices/light.py
@@ -0,0 +1,16 @@
+"""Platform for light integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the light platform for Virtual Devices."""
+    # Currently no entities are added. This will be expanded later.
+    pass
