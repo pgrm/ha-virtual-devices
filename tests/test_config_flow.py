257080a134c33b2ea8a715e40a3fb51343540d1b
Peter Grman <peter.grman@gmail.com>
2025-10-09 23:43:44 +0200

    Consider simplifying the Integration mock.

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -5,7 +5,6 @@ from unittest.mock import patch
 from homeassistant import config_entries, setup
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
-from homeassistant.loader import Integration
 
 from custom_components.virtual_devices.const import DOMAIN
 
@@ -14,22 +13,11 @@ async def test_form(hass: HomeAssistant) -> None:
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
 
-    with patch(
-        "homeassistant.loader.async_get_integrations",
-        return_value={
-            DOMAIN: Integration(
-                hass,
-                "custom_components.virtual_devices",
-                None,
-                {"name": "Virtual Devices", "domain": DOMAIN, "requirements": []},
-            )
-        },
-    ):
-        result = await hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": config_entries.SOURCE_USER}
-        )
-        assert result["type"] is FlowResultType.FORM
-        assert result["errors"] is None
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
 
     with patch(
         "homeassistant.config_entries.ConfigEntries.async_setup",
