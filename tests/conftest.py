34f32d621658d314802c71b957d308b52347d4e1
Peter Grman <peter.grman@gmail.com>
2025-10-10 00:41:39 +0200

    Fix the fixture to actually enable custom integrations.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations defined in the test dir."""
-    yield
+    return enable_custom_integrations
 
 
 @pytest.fixture
